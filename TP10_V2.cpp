18c5b19d2800c49155edbfbe78f574748819f4fb
Ewan Acemyan De Oliveira <127121274+WNCMYN@users.noreply.github.com>
2024-12-17 17:00:34 +0100

    Update TP10_V2.cpp

--- TP10_V2.cpp
+++ TP10_V2.cpp
@@ -16,6 +16,8 @@
 #include <fcntl.h>
 #include <vector>
 #include <csignal>
+#include "source/AStar.hpp"
+
 
 using namespace std;
 
@@ -100,6 +102,21 @@ void  showMatrix (const CMatrix & Mat,bool & rejouer){
 }
 unsigned randomRange (const unsigned& min, const unsigned& max) {
     return min + rand() % (max-min);
+}
+
+CPosition ia(CMatrix & Mat, CPosition & posPlayer2, CPosition & posPlayer1){
+    AStar::Generator generator;
+    generator.setWorldSize({Mat.size(), Mat[0].size()});
+    generator.setDiagonalMovement(false);
+    auto coup = generator.findPath({posPlayer1.first, posPlayer1.second}, {posPlayer2.first, posPlayer2.second});
+    return {coup[1].x, coup[1].y};
+}
+
+void iaMove(CMatrix & Mat, CPosition & posPlayer2, CPosition & posPlayer1){
+    char element = Mat[posPlayer2.first][posPlayer2.second];
+    Mat[posPlayer2.first][posPlayer2.second] = kEmpty;
+    posPlayer2=ia(Mat, posPlayer2, posPlayer1);
+    Mat[posPlayer2.first][posPlayer2.second] = element;
 }
 
 void initMat (CMatrix & Mat, unsigned nbLine, unsigned nbColumn, CPosition & posPlayer1, CPosition & posPlayer2){
