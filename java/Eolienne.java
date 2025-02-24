d20223dd9c663718e62881965130c60de8e5b416
Théo GHEUX <96632622+TheoGHEUX@users.noreply.github.com>
2025-02-24 16:57:10 +0100

    Create Eolienne.java

---
+++ java/Eolienne.java
@@ -0,0 +1,21 @@
+package exoengins;
+
+/**
+ * Class Eolienne
+ */
+
+public class Eolienne extends Propulsion { // Save as "Eolienne.java"
+	
+	public Eolienne() {
+		
+	}
+	
+	public double consommerEnergie() {
+		return 0 ;
+	}
+	
+	public String toString() {
+		return "Eolienne[]" ;
+	}
+	
+}
